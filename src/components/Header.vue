b2661f0bd1419869b853386eeaf8d798673b41dc
yoyo <yoyo_tl009@yahoo.co.jp>
2020-03-18 23:54:10 +0900

    add new page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,8 @@
   <div class="header">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/Character">Character</router-link>
     </div>
   </div>
 </template>
---
+++ src/views/Character.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="character">
+    <h1>My original characters</h1>
+    <p>You can click links and see character sheets</p>
+    <p>キャラクターの名前をクリックすると資料が閲覧できます。</p>
+  </div>
+</template>
